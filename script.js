edbe679b63b483733648dc2395b4141a1498abb3
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-07 16:58:53 +0100

    space between numbers

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ for (let i = 1; i < 101; i++){
     const multipleOfFive = (i % 5) === 0;
     if (multipleOfThree && multipleOfFive) {
         let i = fizz + buzz;
-        numbers += i
+        numbers += " " + i;
     } else if (multipleOfFive) {
         let i = buzz;
-        numbers += i
+        numbers += " " + i;
     } else if (multipleOfThree) {
         let i = fizz;
-        numbers += i
+        numbers += " " + i;
     } else {
-        numbers += i
+        numbers += " " + i;
     }
 };
 
-containerElement.innerText = numbers
+containerElement.innerText = numbers;
 console.log(numbers);
