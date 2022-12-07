6f51030287423f0207ecb3db1508f694f349f235
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-07 16:08:35 +0100

    program logic (print in console)

---
+++ script.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 100; i++){
+    const fizz = "Fizz";
+    const buzz = "Buzz";
+    const multipleOfThree = (i % 3) === 0;
+    const multipleOfFive = (i%5) === 0;
+    if (multipleOfThree && multipleOfFive) {
+        let i = fizz + buzz;;
+        console.log(i);
+    } else if (multipleOfFive) {
+        let i = buzz;
+        console.log(i);
+    } else if (multipleOfThree) {
+        let i = fizz;
+        console.log(i);
+    } else {
+        console.log(i + 1);
+    }
+};
