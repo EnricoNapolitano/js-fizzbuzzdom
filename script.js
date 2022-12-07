54f13a53217905b37813435a80aaae53638a3fdb
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-07 16:47:46 +0100

    result printed on page

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
+const containerElement = document.getElementById("container");
+let numbers = '';
+
 for (let i = 1; i < 101; i++){
     const fizz = "Fizz";
     const buzz = "Buzz";
     const multipleOfThree = (i % 3) === 0;
     const multipleOfFive = (i % 5) === 0;
     if (multipleOfThree && multipleOfFive) {
-        let i = fizz + buzz;;
-        console.log(i);
+        let i = fizz + buzz;
+        numbers += i
     } else if (multipleOfFive) {
         let i = buzz;
-        console.log(i);
+        numbers += i
     } else if (multipleOfThree) {
         let i = fizz;
-        console.log(i);
+        numbers += i
     } else {
-        console.log(i);
+        numbers += i
     }
 };
+
+containerElement.innerText = numbers
+console.log(numbers);
