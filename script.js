d1d51a1b7a6e513f8720d4acb2059277c2283435
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-07 17:01:43 +0100

    moved on top general variables

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const containerElement = document.getElementById("container");
+const fizz = "Fizz";
+const buzz = "Buzz";
 let numbers = '';
 
 for (let i = 1; i < 101; i++){
-    const fizz = "Fizz";
-    const buzz = "Buzz";
-    const multipleOfThree = (i % 3) === 0;
     const multipleOfFive = (i % 5) === 0;
+    const multipleOfThree = (i % 3) === 0;
     if (multipleOfThree && multipleOfFive) {
         let i = fizz + buzz;
         numbers += " " + i;
