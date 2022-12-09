d1466b3b401b715ba0750353ce4f0b3f45490177
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2022-12-09 19:09:29 +0100

    better code order

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
+//* Elements from DOM
 const daysWrap = document.getElementById("days-wrap");
 const buttonElement = document.querySelector("button");
 const inputElement = document.querySelector("input");
 const answerElement = document.getElementById("answer");
-let daysNumber = `<ul>`;
 
-// input interaction
+
+
+//* Variables with message in it
+const correct = `<span class="yellow">Bravo, hai indovinato! SEI SPRITZ! Dopo tutto non sono così tanti.</span> Hai vinto ehmm... nulla. Perché dovresti vincere qualcosa? L'importante è partecipare.`;
+const uncorrect = `<span class="red">Mi dispiace, non hai indovinato.</span> Passa il mouse sulle card e conta bene quante volte Buzz beve il drink!`;
+
+//* input interaction
 buttonElement.addEventListener('click', function(){
     /*
     for design reasons I wanted to guarantee maximum expression to the user
     when filling out the form and not limit his experience choosing to a set range of numbers.
     */
-      if (inputElement.value.toLowerCase().includes("non sei") || inputElement.value.toLowerCase().includes("non 6") || inputElement.value.toLowerCase().includes("non ha bevuto sei") || inputElement.value.toLowerCase().includes("non ha bevuto 6")) {
+    if (inputElement.value.toLowerCase().includes("non sei") || inputElement.value.toLowerCase().includes("non 6") || inputElement.value.toLowerCase().includes("non ha bevuto sei") || inputElement.value.toLowerCase().includes("non ha bevuto 6") || inputElement.value.toLowerCase().includes("16") || inputElement.value.toLowerCase().includes("26") || inputElement.value.toLowerCase().includes("36") || inputElement.value.toLowerCase().includes("46") || inputElement.value.toLowerCase().includes("56") || inputElement.value.toLowerCase().includes("66") || inputElement.value.toLowerCase().includes("76") || inputElement.value.toLowerCase().includes("86") || inputElement.value.toLowerCase().includes("96")) {
         inputElement.value= '';
-        answerElement.innerHTML = `<span class="red">Mi dispiace, non hai indovinato.</span> Passa il mouse sulle card e conta bene quante volte Buzz beve il drink!`;
+        answerElement.innerHTML = uncorrect;
     } else if (inputElement.value.toLowerCase().includes("sei") || inputElement.value.includes("6")) {
         inputElement.value= '';
-        answerElement.innerHTML = `<span class="yellow">Bravo, hai indovinato! SEI SPRITZ! Dopo tutto non è così tanto.</span> Hai vinto ehmm... nulla. Perché dovresti vincere qualcosa? L'importante è partecipare.`;
+        answerElement.innerHTML = correct;
     } else {
         inputElement.value= '';
-        answerElement.innerHTML = `<span class="red">Mi dispiace, non hai indovinato.</span> Passa il mouse sulle card e conta bene quante volte Buzz beve il drink!`;
+        answerElement.innerHTML = uncorrect;
     }
 });
 
-for (let i = 1; i < 101; i++){
-    const multipleOfFive = (i % 5) === 0;
-    const multipleOfThree = (i % 3) === 0;
+
+
+//* Variable needed to create ul and li tags
+let daysNumber = `<ul>`;
+
+for (let i = 1; i <= 100; i++){
+    const multipleOfFive = !(i % 5);
+    const multipleOfThree = !(i % 3);
     if (multipleOfThree && multipleOfFive) {
         daysNumber += `<li class="box"><img src="spritz-buzz.png" alt="immagine di Buzz Lightyear che beve uno spritz"></li>`;
     } else if (multipleOfFive) {
